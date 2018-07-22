2d699bd4a35a0949c27d35f1403826ccd0c3b8e7
aewens <email@aewens.com>
2018-07-21 21:59:55 -0500

    Added templates and can post info to the database

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from app import app
+from app import app, db, models
 from flask import render_template
 from twilio.rest import Client
-from flask import Flask, request, session
+from flask import request, redirect, url_for
 from twilio.twiml.messaging_response import MessagingResponse
 
 # Your Account Sid and Auth Token from twilio.com/console
@@ -10,9 +10,43 @@ auth_token = '341b11b5f7a97a727b8c71c2fcc09467'
 client = Client(account_sid, auth_token)
 
 @app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html")
+@app.route("/home")
+def home():
+    return render_template("home.html", active="home")
+
+@app.route("/new", methods=["GET", "POST"])
+def new():
+    if request.method == "POST":
+        name = request.form.get("name")
+        info = dict()
+        attrs = list(request.form.keys())
+        for attr in attrs:
+            props = attr.split(":") if ":" in attr else None
+            if props is None:
+                continue
+
+            if props[0] == "key":
+                key = request.form.get(attr)
+                index = props[1]
+                info[key] = request.form.get("value:%s" % index)
+
+        user = models.User(name=name)
+        for key, value in info.items():
+            user_info = models.Info(key=key, value=str(value))
+            db.session.add(user_info)
+            user.info.append(user_info)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("new"))
+    else:
+        return render_template("new.html", active="new")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        return "Test"
+    else:
+        return render_template("edit.html", active="edit")
 
 @app.route("/sms", methods=['GET', 'POST'])
 def reply():
